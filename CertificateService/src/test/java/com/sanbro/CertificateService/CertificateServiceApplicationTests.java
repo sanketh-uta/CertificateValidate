94e5d2c675f9ec47e655e39279241f41fd27cdfd
sankethbolishetti <sankethuta@gmail.com>
2025-07-21 12:58:55 -0500

    Initial project setup with just assumed dependencies

---
+++ CertificateService/src/test/java/com/sanbro/CertificateService/CertificateServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanbro.CertificateService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CertificateServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
